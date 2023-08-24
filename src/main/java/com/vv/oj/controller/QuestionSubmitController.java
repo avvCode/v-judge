04e96c55dec0e970ddac96d4a10d3296a4c801c1
zyz19 <80141182@qq.com>
2023-08-24 23:21:41 +0800

    新增接口，根据id查询单提交记录

--- src/main/java/com/vv/oj/controller/QuestionSubmitController.java
+++ src/main/java/com/vv/oj/controller/QuestionSubmitController.java
@@ -51,7 +51,6 @@ public class QuestionSubmitController {
         if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        // 登录才能点赞
         final User loginUser = userService.getLoginUser(request);
         long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
         return ResultUtils.success(questionSubmitId);
